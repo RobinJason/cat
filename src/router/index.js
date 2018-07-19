7d4e182b1239e4dee9a6c3895562c4c18469b31a
RobinJason <r.jase@live.com>
2018-07-19 09:57:22 +0800

    新增路由基本配置，添加stylus到package.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import publish from '@/components/publish/publish'
+import diary from '@/components/diary/diary'
 
 Vue.use(Router)
 
+const routes = [
+  { path: '/publish', component: publish },
+  { path: '/diary', component: diary }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: routes
 })
